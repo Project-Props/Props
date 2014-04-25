4a6c0c6dd0757945f42592c1bf89843cbbbea5ba
David Pedersen <david.pdrsn@gmail.com>
2014-04-25 13:38:50 +0200

    extract route object and raise exception when no route matches

--- router.php
+++ router.php
@@ -1,5 +1,26 @@
 <?php
 
+class NoRouteMatches extends Exception {}
+
+class Route {
+  private $path;
+  private $fun;
+
+  public function __construct($path, $fun) {
+    $this->path = $path;
+    $this->fun = $fun;
+  }
+
+  public function matches($path) {
+    return $this->path == $path;
+  }
+
+  public function run() {
+    $fun = $this->fun;
+    $fun();
+  }
+}
+
 class Router {
   private $routes;
 
@@ -7,17 +28,23 @@ class Router {
     $this->routes = [];
   }
 
-  public function define_router($path, $fun) {
-    $this->routes[$path] = $fun;
+  public function define_route($path, $fun) {
+    $this->routes[$path] = new Route($path, $fun);
   }
 
   public function process_request($path) {
-    foreach ($this->routes as $key => $fun) {
-      if ($key == $path) {
-        $fun();
-        break;
+    $this->process_matching_route($path);
+  }
+
+  private function process_matching_route($path) {
+    foreach ($this->routes as $route){
+      if ($route->matches($path)) {
+        $route->run();
+        return;
       }
     }
+
+    throw new NoRouteMatches("No route matches '" . $path . "'");
   }
 }
 
