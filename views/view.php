c6ac01df1d11d045b16d5bdf59110ff74f010c25
David Pedersen <david.pdrsn@gmail.com>
2014-04-27 09:33:29 +0200

    Make env parameter optional to View#render method
    Sometimes it might not be necessary to specify one

--- views/view.php
+++ views/view.php
@@ -4,7 +4,7 @@ class View {
   private $file_path;
   private $env;
 
-  public function __construct($file_path, $env) {
+  public function __construct($file_path, $env = []) {
     $this->env = $env;
     $this->file_path = $file_path;
   }
