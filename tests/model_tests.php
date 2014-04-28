99563527009a901063a609da63d85684a9471320
David Pedersen <david.pdrsn@gmail.com>
2014-04-28 22:05:49 +0200

    add extra assertion in model test, just to be sure

--- tests/model_tests.php
+++ tests/model_tests.php
@@ -5,7 +5,9 @@ require_once("models/prop.php");
 class ModelTests extends PHPUnit_Framework_TestCase {
   public function test_finds_a_record(){
     $prop = Prop::find(1);
+
     $this->assertEquals($prop->id, 1);
+    $this->assertEquals($prop->description, "en dejlig stol");
   }
 }
 
