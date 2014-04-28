1765529b47fba95ce1692d93c597aea42a418c3f
Louise Knudsen <louise-w-knudsen@hotmail.com>
2014-04-28 17:38:40 +0200

    models folder added, prop class added

--- tests/model_tests.php
+++ tests/model_tests.php
@@ -1,27 +1,6 @@
 <?php
 
-require_once("lib/model.php");
-
-class Prop extends Model {
-  const TABLE_NAME = 'Props';
-
-  public $id,
-         $prop_nr,
-         $section_id,
-         $description,
-         $comment,
-         $date_added,
-         $date_updated,
-         $supplier_id,
-         $price,
-         $bought_for_id,
-         $status_id,
-         $size,
-         $period_id,
-         $deleted,
-         $creditor_id,
-         $maintenance_time;
-}
+require_once("models/prop.php");
 
 class ModelTests extends PHPUnit_Framework_TestCase {
   public function test_finds_a_record(){
