9400cfcb1afd05ff013cc8c2cd5820b3ce848fc2
David Pedersen <david.pdrsn@gmail.com>
2014-04-30 21:29:24 +0200

    uncomment commented out tests

--- tests/model_tests.php
+++ tests/model_tests.php
@@ -10,13 +10,13 @@ class ModelTests extends PHPUnit_Framework_TestCase {
     $this->assertEquals($prop->description, "en dejlig stol");
   }
 
-  // public function test_update_a_record() {
-  //   $prop = Prop::find(1);
-  //   $prop->description = "bar";
-  //   $prop->save();
+  public function test_update_a_record() {
+    $prop = Prop::find(1);
+    $prop->description = "bar";
+    $prop->save();
 
-  //   $this->assertEquals("bar", Prop::find(1)->description);
-  // }
+    $this->assertEquals("bar", Prop::find(1)->description);
+  }
 
   public function test_finds_record_where_id_is_string() {
     $production = Production::find("0000-2014");
@@ -24,17 +24,17 @@ class ModelTests extends PHPUnit_Framework_TestCase {
     $this->assertEquals($production->title, "en dejlig forestilling");
   }
 
-  // public function test_save_new_record() {
-  //   $prop = new Prop();
-  //   $prop->prop_nr = 28;
-  //   $prop->section_id = 1;
-  //   $prop->date_added = '2014-04-30 12:12:12';
-  //   $prop->date_updated = '2014-04-30 12:12:12';
-  //   $prop->status_id = 1;
-  //   $prop->save();
+  public function test_save_new_record() {
+    $prop = new Prop();
+    $prop->prop_nr = 28;
+    $prop->section_id = 1;
+    $prop->date_added = '2014-04-30 12:12:12';
+    $prop->date_updated = '2014-04-30 12:12:12';
+    $prop->status_id = 1;
+    $prop->save();
 
-  //   $this->assertNotNull($prop->id);
-  // }
+    $this->assertNotNull($prop->id);
+  }
 
   public function test_save_new_production() {
     $prod = new Production();
