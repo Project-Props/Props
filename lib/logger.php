6ef9914e618fe5c60cb02d2755521a8988478e1d
David Pedersen <david.pdrsn@gmail.com>
2014-05-02 21:44:26 +0200

    create log file if it doesn't exist

--- lib/logger.php
+++ lib/logger.php
@@ -2,6 +2,11 @@
 
 class Logger {
   public static function log($message, $data = []) {
+    if (!is_file("log/log.log")) {
+      mkdir("log");
+      touch("log/log.log");
+    }
+
     $stream = new Monolog\Handler\StreamHandler('log/log.log', Monolog\Logger::DEBUG);
 
     $log = new Monolog\Logger('log');
