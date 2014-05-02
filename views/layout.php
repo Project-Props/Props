3547f11caff26c8309bfb4c1e4ebb1e01348f21e
David Pedersen <david.pdrsn@gmail.com>
2014-05-02 20:08:40 +0200

    link to normalize.css and jquery
    normalize.css is small css stylesheet that some smart people have
    written. It will normalize the way a lot of standard browser elements
    look on different browsers. This makes it easier to style them since you
    have a uniform base.
    
    jQuery is a javascript library that makes it easier to do all sorts of
    common things.
    
    We are for sure gonna need these two things at some point down the road.

--- views/layout.php
+++ views/layout.php
@@ -4,7 +4,10 @@
     <meta charset="utf-8">
     <title>Props 2.0</title>
 
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
     <script src="assets/js/script.js"></script>
   </head>
   <body>
