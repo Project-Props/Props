2c92ffccf290086e3201c4c1fd4299b64c692c48
David Pedersen <david.pdrsn@gmail.com>
2014-05-02 22:11:07 +0200

    move commas to infront

--- models/prop.php
+++ models/prop.php
@@ -5,22 +5,22 @@ require_once("lib/model.php");
 class Prop extends Model {
   const TABLE_NAME = 'Props';
 
-  public $id,
-         $prop_nr,
-         $section_id,
-         $description,
-         $comment,
-         $date_added,
-         $date_updated,
-         $supplier_id,
-         $price,
-         $bought_for_id,
-         $status_id,
-         $size,
-         $period_id,
-         $deleted,
-         $creditor_id,
-         $maintenance_time;
+  public $id
+        ,$prop_nr
+        ,$section_id
+        ,$description
+        ,$comment
+        ,$date_added
+        ,$date_updated
+        ,$supplier_id
+        ,$price
+        ,$bought_for_id
+        ,$status_id
+        ,$size
+        ,$period_id
+        ,$deleted
+        ,$creditor_id
+        ,$maintenance_time;
 
   protected function has_one() {
     return [
