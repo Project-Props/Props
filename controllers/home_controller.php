d250e346df7466109224f7398b20cb9b52e987c3
David Pedersen <dap@html24.net>
2014-05-10 23:06:52 +0200

    add todo note

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -2,7 +2,10 @@
 
 class HomeController {
   public function index() {
+    // TODO: make this load only the latest props
     $latest_props = Prop::all();
+
+    // TODO: make this load only the latest productions
     $latest_productions = Production::all();
 
     $view = new View("home/index.php", ["props" => $latest_props,
