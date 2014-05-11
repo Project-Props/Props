5e4166c8eeba8fa23853efe8a7c757acd29ba2c6
David Pedersen <dap@html24.net>
2014-05-11 15:41:39 +0200

    reverse props and productions list on home page

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -3,10 +3,10 @@
 class HomeController {
   public function index() {
     // TODO: make this load only the latest props
-    $latest_props = Prop::all();
+    $latest_props = array_reverse(Prop::all());
 
     // TODO: make this load only the latest productions
-    $latest_productions = Production::all();
+    $latest_productions = array_reverse(Production::all());
 
     $view = new View("home/index.php", ["props" => $latest_props,
                                         "productions" => $latest_productions]);
