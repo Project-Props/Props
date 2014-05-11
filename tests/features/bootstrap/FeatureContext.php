6c24cfa627fac1ce9a8fd3f92fc2cbce21b3cb96
David Pedersen <dap@html24.net>
2014-05-11 15:22:49 +0200

    clean the database after each feature test as well

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once("lib/database.php");
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
     Behat\Behat\Context\BehatContext,
@@ -18,28 +23,33 @@ use Behat\MinkExtension\Context\MinkContext;
 /**
  * Features context.
  */
-class FeatureContext extends MinkContext
-{
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
-    }
+class FeatureContext extends MinkContext {
+  /**
+   * Initializes context.
+   * Every scenario gets it's own context object.
+   *
+   * @param array $parameters context parameters (set them up through behat.yml)
+   */
+  public function __construct(array $parameters) {
+    // Initialize your context here
+  }
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }
-//
+  /**
+   * @AfterSuite
+   */
+  public static function teardown($event) {
+    static::reset_db();
+  }
+
+  /** @BeforeFeature */
+  public static function setupFeature($event) {
+    static::reset_db();
+  }
+
+  private static function reset_db() {
+    $db = new Database(new LocalDatabaseConnection());
+    $db->drop_database();
+    $db->create_schema();
+    $db->add_test_data();
+  }
 }
