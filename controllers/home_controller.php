f16288f93fd1089886416c766f41d2ae155ca630
David Pedersen <dap@html24.net>
2014-05-12 13:53:45 +0200

    show props and productions in reversed order on frontpage

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -2,9 +2,9 @@
 
 class HomeController {
   public function index() {
-    $latest_props = array_slice(Prop::all(), -10, 10);
+    $latest_props = array_reverse(array_slice(Prop::all(), -10, 10));
 
-    $latest_productions = array_slice(Production::all(), -10, 10);
+    $latest_productions = array_reverse(array_slice(Production::all(), -10, 10));
 
     $view = new View("home/index.php", ["props" => $latest_props,
                                         "productions" => $latest_productions]);
