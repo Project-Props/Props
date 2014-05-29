cefc83f578eb5276f3f436164f3dfdad5e356004
David Pedersen <david.pdrsn@gmail.com>
2014-05-29 15:00:08 +0200

    update front page with prettier tables and rows

--- views/home/index.php
+++ views/home/index.php
@@ -1,15 +1,21 @@
-<?php if (sizeof($props) == 0): ?>
-  There are no props
-<?php else: ?>
-  <h1>Nyeste props</h1>
+<div class="row">
+  <div class="col-sm-6">
+    <?php if (sizeof($props) == 0): ?>
+      There are no props
+    <?php else: ?>
+      <h1>Nyeste props</h1>
 
-  <?php $this->render_partial("home/_prop_table.php", ["props" => $props]); ?>
-<?php endif; ?>
+      <?php $this->render_partial("home/_prop_table.php", ["props" => $props]); ?>
+    <?php endif; ?>
+  </div>
 
-<?php if (sizeof($productions) == 0): ?>
-  There are no productions
-<?php else: ?>
-  <h1>Forestillinger i repertoire</h1>
+  <div class="col-sm-6">
+    <?php if (sizeof($productions) == 0): ?>
+      There are no productions
+    <?php else: ?>
+      <h1>Forestillinger i repertoire</h1>
 
-  <?php $this->render_partial("home/_production_table.php", ["productions" => $productions]); ?>
-<?php endif; ?>
+      <?php $this->render_partial("home/_production_table.php", ["productions" => $productions]); ?>
+    <?php endif; ?>
+  </div>
+</div>
